1f3159c8028cc703e14c7dc63e06a92a0ba1ef20
Norbert Majubu <majubunorbert@gmail.com>
2017-06-10 17:17:34 +0200

    Project started

---
+++ AiMotiveTest/AiMotiveTest.cpp
@@ -0,0 +1,12 @@
+// AiMotiveTest.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <iostream>
+
+int main()
+{
+	std::cout << "hello world";
+    return 0;
+}
+
